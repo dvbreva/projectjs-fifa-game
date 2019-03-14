efe67760924d769b71c586479b9b232cd3c17cdf
dvbreva <38948264+dvbreva@users.noreply.github.com>
2019-03-14 17:53:06 +0200

    added custom ajax library

---
+++ AjaxWrapper.js
@@ -0,0 +1,27 @@
+var AjaxWrapper = {
+
+    ajax : null,
+
+    init() {
+        this.ajax = new XMLHttpRequest();  
+        return this.ajax;
+    },
+ 
+    get(url, callback) {
+        var request = this.init();
+        request.open("GET", url)
+        request.send();
+        request.onload = () => {
+            callback(JSON.parse(request.responseText));
+        };
+    },
+ 
+    post(url, data, callback) {
+        var request = this.init();
+        request.open("POST", url);
+        request.send(data);
+        request.onload = () => {
+            callback(JSON.parse(request.responseText));
+        };
+    }
+ };
